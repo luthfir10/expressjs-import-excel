78eb9f99db9698bd437f64ffe6c67a7b7ac5acc7
luthfir10 <luthfirr10@gmail.com>
2020-03-04 15:52:33 +0700

    save to mongodb
    coding save ke dalam database setelah pembacaan data excel

--- routes/posts.js
+++ routes/posts.js
@@ -1,37 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Posts');
-
+const Post = require("../models/Posts");
+let upload = require("express-fileupload");
+let importExcel = require("convert-excel-to-json");
+let del = require("del");
 
 //GET back all the posts
-router.get('/', async (req, res)=>{
-    try{
-        const posts = await Post.find();
-        res.json(posts);
-    }catch(err){
-        res.json({messafe: err});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ messafe: err });
+  }
 });
-
 
 //SUBMIT  a posts
-router.post('/', async (req, res) => {
-    const post = new Post({
-        nopes : req.body.nopes,
-        nm_pes : req.body.nm_pes
-    });
+router.post("/", async (req, res) => {
+  let file = req.files.filename;
+  let filename = file.name;
+  const post = {};
 
-    try{
-        const savedPost = await post.save();
-        res.json(savedPost);
-    }catch(err){
-
-        res.json({message: err});
+  file.mv("./excel/" + filename, err => {
+    if (err) {
+      res.send("Maaf gagal upload");
+    } else {
+      let result = importExcel({
+        sourceFile: "./excel/" + filename,
+        header: { rows: 1 },
+        columnToKey: { H: "nopes", K: "nm_pes" },
+        sheets: ["Sheet1"]
+      });
+      for (var i = 0; result.Sheet1.length > i; i++) {
+        post = new Post({
+          nopes: result.Sheet1[i].nopes,
+          nm_pes: result.Sheet1[i].nm_pes
+        });
+        post.save();
+      }
+      res.send(siswa);
+      console.log(siswa + " Jumlah data " + siswa.length);
+      del(["excel/" + filename]).then(paths => {
+        console.log("file " + filename + " sudah di hapus!!");
+      });
     }
+  });
 
+  // const post = new Post({
+  //     nopes : req.body.nopes,
+  //     nm_pes : req.body.nm_pes
+  // });
+
+  // try{
+  //     const savedPost = await post.save();
+  //     res.json(savedPost);
+  // }catch(err){
+
+  //     res.json({message: err});
+  // }
 });
-
-
-
 
 module.exports = router;
